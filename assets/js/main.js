e0afe8ac98791c8766d92739b10b4bae163ea951
Rosario Garufi <saro.garufi1@gmail.com>
2022-04-05 15:21:28 +0200

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*
+
+L'utente indica un livello di difficoltà in base al quale 
+viene generata una griglia di gioco quadrata, 
+in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso 
+range della difficoltà prescelta: le bombe :bomba:.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l'utente clicca su una cella:
+se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba
+la cella si colora di rosso e la partita termina,
+altrimenti la cella cliccata si colora di azzurro e 
+l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando:
+ il giocatore clicca su una bomba
+o raggiunge il numero massimo possibile di numeri consentiti.
+
+*/
+
+
+//selezione select
+
+document.querySelector("form").addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(this);
+})
